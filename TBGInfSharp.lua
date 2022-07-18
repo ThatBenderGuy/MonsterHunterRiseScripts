6e72902cb0031ae890af8490d2f363af1ef25641
ThatBenderGuy <thatbenderguy@yahoo.com>
2022-07-18 16:17:01 -0700

    Initial commit

---
+++ TBGInfSharp.lua
@@ -0,0 +1,54 @@
+require("TBGHelperScripts/helpers")
+
+local config = {
+    enable = false
+}
+
+local configPath = "TBG_Infinite_Sharpness.json"
+
+-- Handle Json config
+if json ~= nil then
+    file = json.load_file(configPath)
+    if file ~= nil then
+        config = file
+    else
+        json.dump_file(configPath, config)
+    end
+end
+
+function handleSharpnessDec()
+    local _, weaponName = getCurrentWeaponInstanceAndName()
+    if weaponName == "Bow" or weaponName == "LightBowgun" or weaponName == "HeavyBowgun" then return end
+
+    local playerBase = getPlayer()
+
+    -- Raw values
+    local maxSharpness = playerBase:call("get_SharpnessGaugeMax")
+   
+    playerBase:call("set_SharpnessGauge", maxSharpness)
+end
+
+sdk.hook(PlayerBase_typedef:get_method("update"), 
+function(args)
+        if config.enable then
+            handleSharpnessDec() 
+        end
+end, 
+function(retval) 
+    return retval 
+end) 
+
+-- UI
+re.on_draw_ui(function()
+    
+    if imgui.tree_node("TBG's Infinite Sharpness") then
+        changed, value = imgui.checkbox("Enabled", config.enable)
+        if changed then
+            config.enable = value
+            json.dump_file(configPath, config)
+        end
+
+        imgui.tree_pop()
+    end
+
+end)
